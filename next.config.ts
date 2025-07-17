35f71c28a2da789c3f2bc3ce9e5f3cdb76e419aa
alperen2 <aalperendurmuss@gmail.com>
2025-07-18 02:27:41 +0300

    fixed

--- next.config.ts
+++ next.config.ts
@@ -6,9 +6,6 @@ const nextConfig: NextConfig = {
   compress: true,
   poweredByHeader: false,
 
-  // Output directory configuration to avoid permission issues
-  distDir: './.next-build',
-
   // Security headers
   async headers() {
     return [
